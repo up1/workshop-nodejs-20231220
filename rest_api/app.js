27d42ce4bb88208c82071cf61ece68a9ac357ba4
up1 <somkiat.p@gmail.com>
2023-12-20 15:29:23 +0700

    Add error middleware

--- rest_api/app.js
+++ rest_api/app.js
@@ -2,14 +2,20 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
-app.get('/hello', async(req, res) => {
-  // Call external api with axios
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
-    res.json({message: 'Hello World with ' + response.data.name})
-  } catch (error) {
-    res.status(500).json({message: 'Error ' + error.message})
-  }
+app.get('/hello', async (req, res, next) => {
+    // Call external api with axios
+    try {
+        const response = await axios.get('https://jsonplaceholder2.typicode.com/users/1');
+        // Success
+        res.json({ message: 'Hello World with ' + response.data.name })
+    } catch (error) {
+        next(error)
+        // res.status(500).json({ message: 'Error ' + error.message })
+    }
+})
+
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: 'Error ' + error.message })
 })
 
 module.exports = app
