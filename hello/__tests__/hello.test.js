4b023e505dedeaa264ed6ffb4d0f325a05161445
up1 <somkiat.p@gmail.com>
2023-12-20 11:05:38 +0700

    Test format

--- hello/__tests__/hello.test.js
+++ hello/__tests__/hello.test.js
@@ -1,7 +1,14 @@
 const hello = require("../hello");
 
-test("Hello Test", () => {
+test("Hello Test 1", () => {
     // Compare = expect, assert, should
     const result = hello.hi("demo");
     expect(result).toBe("Say hi demo");
+});
+
+describe("Hello Test 2", () => {
+    it("should say hi demo", () => {
+        const result = hello.hi("demo");
+        expect(result).toBe("Say hi demo");
+    });
 });
