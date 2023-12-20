5b5f824486daed9ac4b1f8109a1d4493daf84401
up1 <somkiat.p@gmail.com>
2023-12-20 10:05:50 +0700

    Hello nodejs

---
+++ hello/hello.js
@@ -0,0 +1,12 @@
+function hi(name) {
+    return "Say hi " + name;
+}
+
+const hi2 = (name) => {
+    return "Say hi " + name;
+}
+
+module.exports = {
+    hi,
+    hi2
+}
---
+++ hello/index.js
@@ -0,0 +1,4 @@
+const hello = require("./hello");
+
+console.log(hello.hi("Somkiat"));
+console.log(hello.hi2("Somkiat"));
