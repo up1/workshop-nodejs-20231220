e96387a5be21d6f7547aa74f4da8f41cd887f343
up1 <somkiat.p@gmail.com>
2023-12-20 10:14:44 +0700

    More basic

--- hello/hello.js
+++ hello/hello.js
@@ -1,9 +1,9 @@
 function hi(name) {
-    return "Say hi " + name;
+    return `Say hi ${name}`;
 }
 
 const hi2 = (name) => {
-    return "Say hi " + name;
+    return `Say hi ${name}`;
 }
 
 module.exports = {
--- hello/index.js
+++ hello/index.js
@@ -2,3 +2,13 @@ const hello = require("./hello");
 
 console.log(hello.hi("Somkiat"));
 console.log(hello.hi2("Somkiat"));
+
+// var vs let vs const
+// = assign value
+// == compare value
+// ===  compare value and type
+let x = 1;
+x = "dddd";
+x = 1.0000123
+x = true;
+console.log(x);
