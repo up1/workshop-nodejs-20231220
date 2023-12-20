ad73dad5ea657f3af94e8c0c810305d9897575f6
up1 <somkiat.p@gmail.com>
2023-12-20 15:46:33 +0700

    Add logging in middleware

--- rest_api/app.js
+++ rest_api/app.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/hello', async (req, res, next) => {
     // Call external api with axios
     try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+        const response = await axios.get('https://jsonplaceholder2.typicode.com/users/1');
         // Success
         res.json({ message: 'Hello World with ' + response.data.name })
     } catch (error) {
@@ -18,6 +18,7 @@ app.get('/hello', async (req, res, next) => {
 
 // Error handler
 const middlewares = require('./middlewares')
+app.use(middlewares.errorLog)
 app.use(middlewares.errorHandler)
 
 module.exports = app
--- rest_api/middlewares.js
+++ rest_api/middlewares.js
@@ -1,7 +1,13 @@
+const errorLog = (error, req, res, next) => {
+    console.log("Error log : " + error.message)
+    next(error)
+}
+
 const errorHandler = (error, req, res, next) => {
     res.status(500).json({ message: 'Error ' + error.message })
 }
 
 module.exports = {
-    errorHandler
+    errorHandler,
+    errorLog
 }
