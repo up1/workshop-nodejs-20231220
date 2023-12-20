c4685afb2daf04f3415e86c714603e2254352183
up1 <somkiat.p@gmail.com>
2023-12-20 14:19:41 +0700

    Mock api with nock

--- rest_api/__tests__/hello.api.test.js
+++ rest_api/__tests__/hello.api.test.js
@@ -1,7 +1,14 @@
 const request = require('supertest')
+const nock = require('nock')
 const app = require('../app')
 
 test("Success case with /hello", async () => {
+    // Mocking external api
+    nock('https://jsonplaceholder.typicode.com')
+        .get('/users/1')
+        .reply(200, {
+            name: 'Somkiat'
+        })
 
     // Step 1 :: Start server
     // Step 2 :: Send request to /hello
@@ -10,5 +17,5 @@ test("Success case with /hello", async () => {
     // Step 3 :: Verify response (200, message=Hello World!)
     expect(response.status).toEqual(200);
     expect(response.body.message).toEqual('Hello World with Somkiat');
-    expect(response.body).toEqual({message: 'Hello World with Somkiat'});
+    expect(response.body).toEqual({ message: 'Hello World with Somkiat' });
 });
