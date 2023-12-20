55c98bcbb1c12744bc5a1f692558ff76ab6fa818
up1 <somkiat.p@gmail.com>
2023-12-20 14:31:24 +0700

    Fix code

--- rest_api/app.js
+++ rest_api/app.js
@@ -4,8 +4,12 @@ const app = express()
 
 app.get('/hello', async(req, res) => {
   // Call external api with axios
-  const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
-  res.json({message: 'Hello World with ' + response.data.name})
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+    res.json({message: 'Hello World with ' + response.data.name})
+  } catch (error) {
+    res.status(500).json({message: 'Error ' + error.message})
+  }
 })
 
 module.exports = app
