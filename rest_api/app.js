044b14317420ada168bfbffe1df07aaf6a964120
up1 <somkiat.p@gmail.com>
2023-12-20 14:05:36 +0700

    Call api with axios

--- rest_api/app.js
+++ rest_api/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 
-app.get('/hello', (req, res) => {
-  res.json({message: 'Hello World!'})
+app.get('/hello', async(req, res) => {
+  // Call external api with axios
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
+  res.json({message: 'Hello World with ' + response.data.name})
 })
 
 module.exports = app
