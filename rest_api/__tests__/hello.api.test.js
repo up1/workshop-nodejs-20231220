1198d7047213ede3c88627061993d081a529aafe
up1 <somkiat.p@gmail.com>
2023-12-20 13:25:47 +0700

    Refactor code :: ready to test

--- rest_api/__tests__/hello.api.test.js
+++ rest_api/__tests__/hello.api.test.js
@@ -1,3 +1,11 @@
+const request = require('supertest')
+
 test("Success case with /hello", async () => {
+
+    // Step 1 :: Start server
+
+    // Step 2 :: Send request to /hello
+
+    // Step 3 :: Verify response (200, message=Hello World!)
     expect(1).toBe(1);
 });
--- rest_api/index.js
+++ rest_api/index.js
@@ -1,11 +1,5 @@
-const express = require('express')
-const app = express()
+const app = require('./app')
 const port = 3000
-
-app.get('/hello', (req, res) => {
-  res.json({message: 'Hello World!'})
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
