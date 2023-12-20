a13a86c8edbdba5c2e684e86a2681b9e90d7fd89
up1 <somkiat.p@gmail.com>
2023-12-20 11:26:16 +0700

    Format code with prettier

--- hello/__tests__/hello.test.js
+++ hello/__tests__/hello.test.js
@@ -1,14 +1,14 @@
-const hello = require("../hello");
+const hello = require('../hello')
 
-test("Hello Test 1", () => {
+test('Hello Test 1', () => {
     // Compare = expect, assert, should
-    const result = hello.hi("demo");
-    expect(result).toBe("Say hi demo");
-});
+    const result = hello.hi('demo')
+    expect(result).toBe('Say hi demo')
+})
 
-describe("Hello Test 2", () => {
-    it("should say hi demo", () => {
-        const result = hello.hi("demo");
-        expect(result).toBe("Say hi demo");
-    });
-});
+describe('Hello Test 2', () => {
+    it('should say hi demo', () => {
+        const result = hello.hi('demo')
+        expect(result).toBe('Say hi demo')
+    })
+})
--- hello/index.js
+++ hello/index.js
@@ -1,14 +1,19 @@
-const hello = require("./hello");
+const hello = require('./hello')
 
-console.log(hello.hi("Somkiat"));
-console.log(hello.hi2("Somkiat"));
+console.log(hello.hi('Somkiat'))
+console.log(hello.hi2('Somkiat'))
 
 // var vs let vs const
 // = assign value
 // == compare value
 // ===  compare value and type
-let x = 1;
-x = "dddd";
-x = 1.0000123;
-x = true;
-console.log(x);
+let x = 1
+x = 'dddd'
+x = 1.0000123
+x = true
+console.log(x)
+
+const data = {
+    id: 1,
+    name: 'somkiat',
+}
