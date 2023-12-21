b70f88fb46b5576367de4febfc92c604ed04623a
up1 <somkiat.p@gmail.com>
2023-12-21 13:37:26 +0700

    Manage config with dotenv

--- rest_api/database.js
+++ rest_api/database.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const pg = require('pg')
 const pool = new pg.Pool({
-    host: 'localhost',
-    database: 'demo',
-    user: 'user',
-    password: 'password',
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     port: 5432,
 });
 
